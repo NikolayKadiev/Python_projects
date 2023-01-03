e009694af7e4d17842b5b888e1627662c9d1641e
NikolayKadiev <82579213+NikolayKadiev@users.noreply.github.com>
2023-01-03 13:41:47 +0200

    Update servo_control.py

--- servo_control/servo_control.py
+++ servo_control/servo_control.py
@@ -1,6 +1,13 @@
 from tkinter import *
 import serial
 
+comm_serial = 0
+
+
+def openPort():
+    global comm_serial
+    comm_serial = serial.Serial(portEntry.get(), 115200)
+
 
 def printSlide():
     serv1 = slide1.get()
@@ -10,33 +17,44 @@ def printSlide():
     print('Slide 2 = ' + str(serv2))
     print('Slide 3 = ' + str(serv3))
     print(' ')
-    comm_serial.write(serv1.to_bytes(1, 'big'))
+    comm_serial.write(b'o')
+    comm_serial.write((serv1 >> 8).to_bytes(1, 'little'))
+    comm_serial.write((serv1 & 0xff).to_bytes(1, 'little'))
+    comm_serial.write((serv2 >> 8).to_bytes(1, 'little'))
+    comm_serial.write((serv2 & 0xff).to_bytes(1, 'little'))
+    comm_serial.write((serv3 >> 8).to_bytes(1, 'little'))
+    comm_serial.write((serv3 & 0xff).to_bytes(1, 'little'))
 
 
-view = Tk()
-view.geometry('400x200')
-view.title("Servo Control")
+if __name__ == "__main__":
+    view = Tk()
+    view.geometry('400x220')
+    view.title("Servo Control")
 
-slide1 = Scale(view, from_=0, to=180, orient=HORIZONTAL, length=390, showvalue=FALSE)
-slide2 = Scale(view, from_=0, to=180, orient=HORIZONTAL, length=390, showvalue=FALSE)
-slide3 = Scale(view, from_=0, to=180, orient=HORIZONTAL, length=390, showvalue=FALSE)
+    slide1 = Scale(view, from_=1000, to=4800, orient=HORIZONTAL, length=390, showvalue=FALSE)
+    slide2 = Scale(view, from_=1000, to=4758, orient=HORIZONTAL, length=390, showvalue=FALSE)
+    slide3 = Scale(view, from_=1000, to=4758, orient=HORIZONTAL, length=390, showvalue=FALSE)
 
-butt1 = Button(view, text='Send data', command=printSlide)
+    butt1 = Button(view, text='Send data', command=printSlide)
+    butt2 = Button(view, text='Open Port', command=openPort)
 
-label1 = Label(view, text='Servo 1')
-label2 = Label(view, text='Servo 2')
-label3 = Label(view, text='Servo 3')
+    label1 = Label(view, text='Servo 1')
+    label2 = Label(view, text='Servo 2')
+    label3 = Label(view, text='Servo 3')
+    portEntry = Entry(view, width=20)
 
-label1.grid(row=1)
-label2.grid(row=3)
-label3.grid(row=5)
+    portEntry.grid(row=1)
+    butt2.grid(row=2)
 
-slide1.grid(row=2)
-slide2.grid(row=4)
-slide3.grid(row=6)
+    label1.grid(row=3)
+    label2.grid(row=5)
+    label3.grid(row=7)
 
-butt1.grid(row=7)
+    slide1.grid(row=4)
+    slide2.grid(row=6)
+    slide3.grid(row=8)
 
-comm_serial = serial.Serial('COM4', 115200)
+    butt1.grid(row=9)
 
-view.mainloop()
+    view.mainloop()
+    comm_serial.close()
